97b227c3b30d41b24650da15b2fb75375e9b0861
nghugo <nghugoryan@gmail.com>
2023-07-15 10:23:30 +0800

    create base, home html

--- poll/urls.py
+++ poll/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("base/", views.base, name="poll-base"),
     path("", views.home, name="poll-home")
 ]
--- poll/views.py
+++ poll/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def base(request):
+    return render(request, "poll/base.html")
+
 def home(request):
     return render(request, "poll/home.html")
+    
