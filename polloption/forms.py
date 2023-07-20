750e35573470d58aa80cd194665e6877b44f2e58
nghugo <nghugoryan@gmail.com>
2023-07-20 16:20:36 +0800

    added data-posting Formset(Form). Not connected to model yet (not Formset of ModelForm), but data is being POSTed.

--- polloption/forms.py
+++ polloption/forms.py
@@ -1,42 +1,67 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, BaseFormSet, Form
 from django.core.exceptions import ValidationError
 from .models import PollOption
 from poll.models import Poll
 
 
-class PollOptionCreateForm(ModelForm):
+# class PollOptionCreateForm(ModelForm):  # temp disable DEBUG
+class PollOptionCreateForm(Form):
+
     class Meta:
         model = PollOption
         fields = ["poll_id", "event_start_time", "event_end_time"]
         # poll_id is a hiddenfield to be filled in using poll_id in url
         # to prevent tampering, also check for permission (against poll password of poll_id)        
 
-        widgets = {
-            'event_start_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-            'event_end_time':  forms.DateTimeInput(attrs={'type': 'datetime-local'})     
-        }
+        # temp disable DEBUG
+        # widgets = {
+        #     'event_start_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        #     'event_end_time':  forms.DateTimeInput(attrs={'type': 'datetime-local'})     
+        # }
     
     queryset = Poll.objects.all()  # MODIFY TO CONTROL POLL PASSWORD BASED PERMISSIONS
-
     poll_id = forms.ModelChoiceField(queryset = queryset, disabled=True)  # disabled=True keeps the initial value in form, and prevents tampering
     # poll_id = forms.ModelChoiceField(queryset = queryset, disabled=True, widget = forms.HiddenInput())
 
-    def clean(self):
-        cleaned_data = super().clean()
-        event_start_time = cleaned_data.get("event_start_time")
-        event_end_time = cleaned_data.get("event_end_time")
+    # temp disable DEBUG
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     event_start_time = cleaned_data.get("event_start_time")
+    #     event_end_time = cleaned_data.get("event_end_time")
         
-        if not (event_start_time and event_end_time):
-            raise ValidationError('Please enter both event start time and event end time')
+    #     print("***************************")
+    #     for key, val in cleaned_data.items():
+    #         print(f"{key} received as {val}")
+        
+    #     if not (event_start_time and event_end_time):
+    #         raise ValidationError('Please enter both event start and end times')
 
-        if event_start_time > event_end_time:
-            raise ValidationError('Event must start before end')
+    #     if event_start_time > event_end_time:
+    #         raise ValidationError('Event must start before end')
         
         # also need to check for poll permission against poll_id here
         # TO IMPLEMENT CODE
 
-# Use a formset to repeat PollOptionCreateForm
 
-PollOptionCreateFormSet = forms.formset_factory(PollOptionCreateForm, extra=0)
+
+class BasePollOptionCreateFormSet(BaseFormSet):
+    def clean(self):
+        """Checks that no two articles have the same title."""
+        if any(self.errors):
+            # Don't bother validating the formset unless each form is valid on its own
+            print('error in form')
+            return
+        startEndPairs = []
+        print("*********************")
+        print("pairs received as:")
+        for form in self.forms:
+            start = form.cleaned_data.get("start")
+            end = form.cleaned_data.get("end")
+            startEndPairs.append((start, end))
+        print(startEndPairs)
+
+PollOptionCreateFormSet = forms.formset_factory(PollOptionCreateForm, extra=0, max_num=10)
+# PollOptionCreateFormSet = forms.formset_factory(PollOptionCreateForm, formset=BasePollOptionCreateFormSet)
+# PollOptionCreateFormSet = forms.formset_factory(PollOptionCreateForm, extra=0, formset=BasePollOptionCreateFormSet)
 # using initial data with formset https://docs.djangoproject.com/en/4.2/topics/forms/formsets/#using-initial-data-with-a-formset
--- polloption/views.py
+++ polloption/views.py
@@ -1,5 +1,7 @@
+import datetime
+
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.hashers import make_password
 from django.urls import reverse, reverse_lazy
 from passlib.handlers.django import django_pbkdf2_sha256
@@ -10,30 +12,38 @@ from .models import PollOption
 from poll.models import Poll
 
 
-class PollOptionCreateView(CreateView):
-    model = PollOption
+class PollOptionCreateView(FormView):  # CreateView later DEBUG with FormView now
+    # model = PollOption
     form_class = PollOptionCreateForm
     template_name = 'polloption/polloption_create.html'  # override default polloption/polloption_form.html'
     
     def get_initial(self):
-        return {'poll_id':Poll.objects.get(pk=self.request.GET.get('poll_id'))}
+        return {'poll_id': Poll.objects.get(pk=self.request.GET.get('poll_id'))}
     
     def form_valid(self, form):  # Called when valid form data has been POSTed, returns HttpResponse
         """ Set poll_id as final field entry again, to prevent tampering """
-        form.instance.poll_id = self.request.GET.get('poll_id')
-        return super().form_valid(form)
-    
-    # # # # # #
-    # Formset #
-    # # # # # #
+        # form.instance.poll_id = self.request.GET.get('poll_id')  # temp disable DEBUG
 
-    def get_context_data(self, **kwargs):  # pass extra context to template
+        print("**************** key, val received as:")
+        for key, val in self.request.POST.items():
+            print(key, val)
+
+        # temp disable DEBUG
+        print("**************** key, val cleaned as:")
+        # cleaned_data = super().clean()
+        # for key, val in cleaned_data.items():
+        #     print(key, val)
+
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):  # pass formset as extra context to template
         context = super().get_context_data(**kwargs)
-        
-        # multiplier controls the number of forms
-        initial=[{'poll_id':Poll.objects.get(pk=self.request.GET.get('poll_id'))}] * 2
+        initial=[{'poll_id':Poll.objects.get(pk=self.request.GET.get('poll_id'))}]
         context["formset"] = PollOptionCreateFormSet(initial=initial)
         return context
+    
+    def get_success_url(self):
+        return reverse("poll-home")
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # #
     #  use form_valid to validate against poll password #
