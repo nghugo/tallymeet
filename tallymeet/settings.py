048fff87e8d2ad33e4673c5a18a0990d4a8a4ae5
nghugo <nghugoryan@gmail.com>
2023-07-30 10:52:47 +0800

    set debug mode to false

--- tallymeet/settings.py
+++ tallymeet/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("TALLYMEET_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = ['tallymeet.onrender.com']
 
