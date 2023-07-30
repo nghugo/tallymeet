382dbbbdfaf652324e976655cfcb9c5a513f73e4
nghugo <nghugoryan@gmail.com>
2023-07-30 10:40:20 +0800

    change allowed host

--- tallymeet/settings.py
+++ tallymeet/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("TALLYMEET_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
