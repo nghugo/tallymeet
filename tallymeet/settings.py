635c8c004faf112a030a9661c3baf40d7a59fdb2
nghugo <nghugoryan@gmail.com>
2023-07-30 10:57:34 +0800

    bug fix allowed hosts

--- tallymeet/settings.py
+++ tallymeet/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("TALLYMEET_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG') == 'True'
+DEBUG = env("DEBUG") == True
 
-ALLOWED_HOSTS = ['tallymeet.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'tallymeet.onrender.com']
 
 
 # Application definition
